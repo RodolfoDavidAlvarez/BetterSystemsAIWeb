3cbf391fbed025a769d09a99cc44ed4a735f0ac3
RodolfoDavidAlvarez <81598038+RodolfoDavidAlvarez@users.noreply.github.com>
2024-12-10 22:54:53 +0000

    I was able to deploy the website finally, no wai just need to make minor adjustments

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,6 @@
 export default {
   plugins: {
-    'postcss-import': {
-      path: ["client/src"]
-    },
-    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
-    autoprefixer: {
-      flexbox: true,
-      grid: 'autoplace',
-      overrideBrowserslist: ['>0.2%', 'not dead', 'not op_mini all']
-    },
-    'cssnano': process.env.NODE_ENV === 'production' ? {
-      preset: ['default', {
-        discardComments: {
-          removeAll: true,
-        },
-        normalizeWhitespace: false,
-        colormin: true,
-        mergeLonghand: true,
-        cssDeclarationSorter: true,
-        reduceIdents: false
-      }],
-    } : false
+    autoprefixer: {},
   },
 }

4c4120f7818fbac17253d94825e5957e62c36938
rodolfodavid110 <38057015-rodolfodavid110@users.noreply.replit.com>
2025-01-14 14:35:33 +0000

    Agent query: Is the website loading correctly and is the photo analysis feature completely removed from the Custom Solutions page?
    Remove photo submission page and related functionality.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/5afe1ed8-5be5-4ed0-beb9-1fae821c0655/5185b85b-60a4-4e28-8653-7b1d64021e43.jpg

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -19,7 +19,6 @@ import PartnersPage from "./pages/PartnersPage";
 import ContactPage from "./pages/ContactPage";
 import Booking from "./components/sections/Booking";
 import SocialPage from "./pages/SocialPage";
-import PhotoSubmissionPage from "./pages/PhotoSubmissionPage";
 import GetStartedPage from "./pages/GetStartedPage";
 import BusinessImpactPage from "./pages/BusinessImpactPage";
 import PreAssessmentQuestionnairePage from "./pages/services/PreAssessmentQuestionnairePage";
@@ -42,7 +41,6 @@ function Router() {
         <Route path="/contact" component={ContactPage} />
         <Route path="/booking" component={Booking} />
         <Route path="/founders-social" component={SocialPage} />
-        <Route path="/photo-submission" component={PhotoSubmissionPage} />
         <Route path="/get-started" component={GetStartedPage} />
         <Route path="/business-impact" component={BusinessImpactPage} />
         <Route path="/services/pre-assessment" component={PreAssessmentQuestionnairePage} />
